666e8e1fcd084ea0581f98914c4f283bd51ef36d
dpisaturo <dpisaturo@4ef8b2a0-b237-0410-b753-5dc98fa25004>
2007-08-16 00:06:56 +0000

    probando commitear algo desde eclipse...

---
+++ src/src/modulo1/modulo1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : modulo1.cpp
+// Author      : Damian
+// Version     :
+// Copyright   : (C) Copyright 2007
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl; // prints Hello World!
+	return 0;
+}
