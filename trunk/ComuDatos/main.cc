92cbcf7adc846189b856dbbe03050c72ba22d729
dpisaturo <dpisaturo@4ef8b2a0-b237-0410-b753-5dc98fa25004>
2007-10-03 18:38:02 +0000

    Documentación: Cronograma, Diseño de la Capa Físca y de la Capa de Índices. Módulo que hizo Ale para comunicar las distintas capas.

---
+++ trunk/ComuDatos/main.cc
@@ -0,0 +1,23 @@
+/* Created by Anjuta version 1.2.4a */
+/*	This file will not be overwritten */
+#include "ComuDatos.h"
+#include <iostream>
+
+int main(int argc, char**argv)
+{
+	string a = argv[0];
+	ComuDatos misComu(argv);
+	
+
+	misComu.ejecutable("./proceso2");
+	
+	
+	misComu.agregarParametro("dos", 1);
+
+	misComu.lanzar();
+	
+	misComu.escribir("Luke soy tu hijo");	
+	cout << endl << "main lee: " << misComu.leer(100) << endl;
+	misComu.liberarRecursos();
+	return 0;
+}
