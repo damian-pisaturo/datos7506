664014cfdf1a16f0febaaa9f74e1cf35718a5d12
manugarciacab <manugarciacab@4ef8b2a0-b237-0410-b753-5dc98fa25004>
2007-10-20 04:53:32 +0000

    No compila aun esto, agregué la clase Codigo y el Nodo es el de Seba.

---
+++ CommonCI/Codigo.h
@@ -0,0 +1,13 @@
+#ifndef CODIGO_H_
+#define CODIGO_H_
+
+class Codigo {
+	
+	public:
+		const static int OK = 0;
+		const static int UNDERFLOW = 1;
+		const static int OVERFLOW = 2;
+		
+};
+
+#endif /*CODIGO_H_*/
