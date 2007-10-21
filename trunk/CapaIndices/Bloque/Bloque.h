917ee6ab74d0c40249ca7d93ce4be779d51b4a18
rdz.maria.laura <rdz.maria.laura@4ef8b2a0-b237-0410-b753-5dc98fa25004>
2007-10-21 07:21:17 +0000

    Se implementaron algunos métodos.

--- trunk/CapaIndices/Bloque/Bloque.h
+++ trunk/CapaIndices/Bloque/Bloque.h
@@ -1,15 +1,60 @@
 #ifndef BLOQUE_H_
 #define BLOQUE_H_
 
+#define OK 0;
+#define FAIL 1;
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <list>
+
+using namespace std;
+
 class Bloque
 {
 private:
 	int numero;
 	int tamanio;
 	char *datos;
+	
+	//Métodos privados
+	
+	/*
+	 * Organiza el bloque a partir de una modificación dentro del mismo
+	 **/
+	
+	void organizarBloque();
+	/*
+	 * Verifica que el registro a insertar tiene espacio dentro del bloque
+	 * Retorna "true" en caso de que haya espacio y "false" en caso contrario
+	 **/
+	bool verificarEspacioDisponible(int longReg,int offsetEspLibre);
+	
+	/*
+	 * Inserta un registro dentro del bloque, actualizando el offset a espacio libre
+	 **/
+	void insertarRegistro(char *registro,int nuevoOffsetEspLibre);
+
 public:
-	Bloque();
+	Bloque(int num,int tam);
 	virtual ~Bloque();
+	
+	/*
+	 * Inserta un nuevo registro dentro del bloque
+	 **/
+	int altaRegistro(char *registro);
+	
+	/*
+	 * Elimina un registro del bloque, reorganizando el espacio libre
+	 **/
+	int bajaRegistro(list <string>listaParam,int longReg);
+	
+	/*
+	 * Modifica el contenido de un registro 
+	 **/
+	int modificarRegistro(list <string>listaParam,int longReg);
+
 };
 
 #endif /*BLOQUE_H_*/
