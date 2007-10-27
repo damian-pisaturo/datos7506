4ce2f799bb2d42991f93c31c9b05dea41d95eb4d
nfantone <nfantone@4ef8b2a0-b237-0410-b753-5dc98fa25004>
2007-10-27 22:17:59 +0000

    Se comenzo la implementacion de los metodos leerBloque y escribirBloque. Se agrego la clase OperacionesCapas.

---
+++ trunk/Common/OperacionesCapas.h
@@ -0,0 +1,13 @@
+#ifndef OPERACIONESCAPAS_H_
+#define OPERACIONESCAPAS_H_
+
+class OperacionesCapas
+{
+	public:
+		const static unsigned char FISICA_ESCRIBIR  = 0;
+		const static unsigned char FISICA_LEER      = 1;
+		const static unsigned char FISICA_MODIFICAR = 2;
+		const static unsigned char FISICA_ELIMINAR  = 3;
+};
+
+#endif /*OPERACIONESCAPAS_H_*/
