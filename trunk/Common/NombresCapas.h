78d9492deae2dea292c36e194d3874aaee85590d
ecaravatti <ecaravatti@4ef8b2a0-b237-0410-b753-5dc98fa25004>
2007-11-03 21:08:27 +0000

    Se crearon defines con los nombres de los ejecutables de las distintas capas.

--- trunk/Common/NombresCapas.h
+++ trunk/Common/NombresCapas.h
@@ -1,21 +1,9 @@
 #ifndef NOMBRESCAPAS_H_
 #define NOMBRESCAPAS_H_
-#include <string>
 
-using namespace std;
+#define NOMBRE_CAPA_FISICA		"capaFisica"
+#define NOMBRE_CAPA_INDICES		"capaIndices"
+#define NOMBRE_CAPA_CONSULTAS	"capaConsultas"
+#define NOMBRE_CAPA_METADATA	"capaMetadata"
 
-class NombreCapas
-{
-	public:
-		const static string CAPA_FISICA;
-		const static string CAPA_INDICES;
-		const static string CAPA_CONSULTAS;
-		const static string CAPA_METADATA;
-};
-/*
-const string NombreCapas::CAPA_FISICA    = "capaFisica";
-const string NombreCapas::CAPA_INDICES   = "capaIndices";
-const string NombreCapas::CAPA_CONSULTAS = "capaConsultas";
-const string NombreCapas::CAPA_METADATA  = "capaMetadata";
-*/
 #endif /*NOMBRESCAPAS_H_*/
