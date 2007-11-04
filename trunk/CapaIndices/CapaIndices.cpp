dfc6a913137522d6ac268f7f16bf11bb11986733
ecaravatti <ecaravatti@4ef8b2a0-b237-0410-b753-5dc98fa25004>
2007-11-04 19:29:45 +0000

    Se creó un modelo de archivo de operaciones.

--- trunk/CapaIndices/CapaIndices.cpp
+++ trunk/CapaIndices/CapaIndices.cpp
@@ -1,21 +1,82 @@
 //#include "CapaIndices.h"
 
 #include <iostream>
+#include <map>
 #include "Bloque/Bloque.h"
 
 using namespace std;
 
-void cargarDefiniciones() {
+typedef map<string, ListaNodos*> MapaDefiniciones;
+
+void cargarDefiniciones(MapaDefiniciones &mapaDef) {
+	
+	ListaNodos* listaNodos = NULL;
+	nodoLista nodo;
+	
+	//CARGO LAS DEFINICIONES DEL TIPO "PERSONA"
+	
+	listaNodos = new ListaNodos();
+	
+	//El primer nodo de la lista indica si el registro es variable o no.
+	nodo.tipo = TipoDatos::TIPO_VARIABLE;
+	nodo.pk = "0";
+	nodo.cantClaves = 1;
+	
+	listaNodos->push_back(nodo);
+	
+	//DNI
+	nodo.tipo = TipoDatos::TIPO_ENTERO;
+	nodo.pk = "true";
+	listaNodos->push_back(nodo);
+	
+	//Nombre
+	nodo.tipo = TipoDatos::TIPO_STRING;
+	nodo.pk = "false";
+	listaNodos->push_back(nodo);
+	
+	//FechaNacimiento
+	nodo.tipo = TipoDatos::TIPO_FECHA;
+	nodo.pk = "false";
+	listaNodos->push_back(nodo);
+	
+	mapaDef["Persona"] = listaNodos;
 	
 	
+	//CARGO LAS DEFINICIONES DEL TIPO "PELICULA"
+
+	listaNodos = new ListaNodos();
 	
+	//El primer nodo de la lista indica si el registro es variable o no.
+	nodo.tipo = TipoDatos::TIPO_FIJO;
+	nodo.pk = "34";
+	nodo.cantClaves = 2;
+	
+	listaNodos->push_back(nodo);
+	
+	//Titulo (cadena de 15 carecteres)
+	nodo.tipo = TipoDatos::TIPO_STRING;
+	nodo.pk = "true";
+	listaNodos->push_back(nodo);
+	
+	//Director (cadena de 10 caracteres)
+	nodo.tipo = TipoDatos::TIPO_STRING;
+	nodo.pk = "true";
+	listaNodos->push_back(nodo);
+	
+	//Genero
+	nodo.tipo = TipoDatos::TIPO_CHAR;
+	nodo.pk = "false";
+	listaNodos->push_back(nodo);
+	
+	mapaDef["Pelicula"] = listaNodos;
 	
 }
 
 int main(int argc, char** argv) {
 	 
+	MapaDefiniciones mapaDef;
 	
-	cargarDefiniciones();
+	cargarDefiniciones(mapaDef);
 	/*
 	 * Este codigo es para probar ABM con registros fijos.
 	 * Cabe aclarar que bloque no es el encargado de chequear las claves repetidas, esa funcionalidad 
