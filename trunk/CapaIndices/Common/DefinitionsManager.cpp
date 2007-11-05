f7e673ef455767d1b720dd934707ca69bf288bf1
manugarciacab <manugarciacab@4ef8b2a0-b237-0410-b753-5dc98fa25004>
2007-11-05 03:28:26 +0000

    Terminada la implementacion de DefinitionsManager.

--- trunk/CapaIndices/Common/DefinitionsManager.cpp
+++ trunk/CapaIndices/Common/DefinitionsManager.cpp
@@ -7,79 +7,264 @@ DefinitionsManager::DefinitionsManager() {
 }
 
 DefinitionsManager::~DefinitionsManager() {
-
+	
 	for (MapaTiposAtributos::iterator iter = this->mapaTiposAtributos.begin();
-		iter != this->mapaTiposAtributos.end(); ++iter) {
-		
+		iter != this->mapaTiposAtributos.end(); ++iter)
+		delete iter->second;
+	
+	for (MapaNombresAtributos::iterator iter = this->mapaNombresAtributos.begin();
+		iter != this->mapaNombresAtributos.end(); ++iter)
+		delete iter->second;
+	
+	ListaTiposIndices* listaTiposIndices;
+	
+	for (MapaTiposIndices::iterator iter = this->mapaTiposIndices.begin();
+		iter != this->mapaTiposIndices.end(); ++iter) {
+		listaTiposIndices = iter->second;
+		for (ListaTiposIndices::iterator iterLista = listaTiposIndices->begin();
+			iterLista != listaTiposIndices->end(); ++iterLista) {
+			delete (*iterLista).listaNombresClaves;
+		}
+		delete listaTiposIndices;
 	}
-			
-	//MapaTiposIndices mapaTiposIndices;
-
 
 }
 
 
 void DefinitionsManager::cargarDefiniciones() {
-	/*
-	ListaNodos* listaNodos = NULL;
+
+	this->cargarNombresAtributos();
+	
+	this->cargarTiposAtributos();
+	
+	this->cargarTiposIndices();
+
+}
+
+
+void DefinitionsManager::cargarNombresAtributos() {
+	
+	//CARGO LOS NOMBRES DE LOS ATRIBUTOS DEL TIPO "PERSONA"
+	
+	ListaNombresAtributos* listaNombresAtributos = new ListaNombresAtributos();
+	
+	listaNombresAtributos->push_back("DNI");
+	listaNombresAtributos->push_back("NOMBRE");
+	listaNombresAtributos->push_back("FECHANACIMIENTO");
+	
+	this->mapaNombresAtributos["PERSONA"] = listaNombresAtributos;
+	
+	
+	//CARGO LOS NOMBRES DE LOS ATRIBUTOS DEL TIPO "PELICULA"
+		
+	listaNombresAtributos = new ListaNombresAtributos();
+	
+	listaNombresAtributos->push_back("TITULO");
+	listaNombresAtributos->push_back("DIRECTOR");
+	listaNombresAtributos->push_back("GENERO");
+	
+	this->mapaNombresAtributos["PELICULA"] = listaNombresAtributos;
+}
+
+
+void DefinitionsManager::cargarTiposAtributos() {
+
+	ListaTiposAtributos* listaTiposAtributos = NULL;
 	nodoLista nodo;
 	
 	//CARGO LAS DEFINICIONES DEL TIPO "PERSONA"
 	
-	listaNodos = new ListaNodos();
+	listaTiposAtributos = new ListaTiposAtributos();
 	
 	//El primer nodo de la lista indica si el registro es variable o no.
 	nodo.tipo = TipoDatos::TIPO_VARIABLE;
 	nodo.pk = "0";
 	nodo.cantClaves = 1;
 	
-	listaNodos->push_back(nodo);
+	listaTiposAtributos->push_back(nodo);
 	
 	//DNI
 	nodo.tipo = TipoDatos::TIPO_ENTERO;
 	nodo.pk = "true";
-	listaNodos->push_back(nodo);
+	listaTiposAtributos->push_back(nodo);
 	
 	//Nombre
 	nodo.tipo = TipoDatos::TIPO_STRING;
 	nodo.pk = "false";
-	listaNodos->push_back(nodo);
+	listaTiposAtributos->push_back(nodo);
 	
 	//FechaNacimiento
 	nodo.tipo = TipoDatos::TIPO_FECHA;
 	nodo.pk = "false";
-	listaNodos->push_back(nodo);
+	listaTiposAtributos->push_back(nodo);
 	
-	mapaDef["Persona"] = listaNodos;
+	this->mapaTiposAtributos["PERSONA"] = listaTiposAtributos;
 	
 	
 	//CARGO LAS DEFINICIONES DEL TIPO "PELICULA"
 
-	listaNodos = new ListaNodos();
+	listaTiposAtributos = new ListaTiposAtributos();
 	
 	//El primer nodo de la lista indica si el registro es variable o no.
 	nodo.tipo = TipoDatos::TIPO_FIJO;
 	nodo.pk = "34";
 	nodo.cantClaves = 2;
 	
-	listaNodos->push_back(nodo);
+	listaTiposAtributos->push_back(nodo);
 	
 	//Titulo (cadena de 15 carecteres)
 	nodo.tipo = TipoDatos::TIPO_STRING;
 	nodo.pk = "true";
-	listaNodos->push_back(nodo);
+	listaTiposAtributos->push_back(nodo);
 	
 	//Director (cadena de 10 caracteres)
 	nodo.tipo = TipoDatos::TIPO_STRING;
 	nodo.pk = "true";
-	listaNodos->push_back(nodo);
+	listaTiposAtributos->push_back(nodo);
 	
 	//Genero
 	nodo.tipo = TipoDatos::TIPO_CHAR;
 	nodo.pk = "false";
-	listaNodos->push_back(nodo);
+	listaTiposAtributos->push_back(nodo);
 	
-	mapaDef["Pelicula"] = listaNodos;
-	*/
+	this->mapaTiposAtributos["PELICULA"] = listaTiposAtributos;
+
+	
+}
+
+
+void DefinitionsManager::cargarTiposIndices() {
+
+	EstructTipoIndice estructTipoIndice;
+	ListaNombresClaves* listaNombresClaves = NULL;
+	NodoListaIndices nodoListaIndices;
+	ListaTiposIndices* listaTiposIndices = NULL;
+	
+	//CARGO LAS DEFINICIONES DE LOS INDICES DEL TIPO "PERSONA"
+	
+	//INDICE GRIEGO (HASH)
+	estructTipoIndice.tipoIndice = TipoIndices::GRIEGO;
+	estructTipoIndice.tipoEstructura = TipoIndices::HASH;
+	estructTipoIndice.tamanioBloque = 4096;
+	
+	listaNombresClaves = new ListaNombresClaves();
+	listaNombresClaves->push_back("DNI");
+	
+	nodoListaIndices.estructTipoIndice = estructTipoIndice;
+	nodoListaIndices.listaNombresClaves = listaNombresClaves;
+	
+	listaTiposIndices = new ListaTiposIndices();
+	listaTiposIndices->push_back(nodoListaIndices);
+	
+	
+	//INDICE ROMANO (ARBOLB*)
+	estructTipoIndice.tipoIndice = TipoIndices::ROMANO;
+	estructTipoIndice.tipoEstructura = TipoIndices::ARBOL_BS;
+	estructTipoIndice.tamanioBloque = 1024;
+	
+	listaNombresClaves = new ListaNombresClaves();
+	listaNombresClaves->push_back("FECHANACIMIENTO");
+	listaNombresClaves->push_back("NOMBRE");
+	
+	nodoListaIndices.estructTipoIndice = estructTipoIndice;
+	nodoListaIndices.listaNombresClaves = listaNombresClaves;
+	
+	listaTiposIndices->push_back(nodoListaIndices);
+	
+	
+	//INDICE ROMANO (ARBOLB+)
+	estructTipoIndice.tipoIndice = TipoIndices::ROMANO;
+	estructTipoIndice.tipoEstructura = TipoIndices::ARBOL_BP;
+	estructTipoIndice.tamanioBloque = 2048;
+	
+	listaNombresClaves = new ListaNombresClaves();
+	listaNombresClaves->push_back("NOMBRE");
+	listaNombresClaves->push_back("FECHANACIMIENTO");
+	
+	nodoListaIndices.estructTipoIndice = estructTipoIndice;
+	nodoListaIndices.listaNombresClaves = listaNombresClaves;
+	
+	listaTiposIndices->push_back(nodoListaIndices);
+	
+	//Se inserta la lista de tipos de indices en el mapa de tipos de indices
+	this->mapaTiposIndices["PERSONA"] = listaTiposIndices;
+
+	
+	//CARGO LAS DEFINICIONES DE LOS INDICES DEL TIPO "PELICULA"
+	
+	//INDICE GRIEGO (ARBOLB*)
+	estructTipoIndice.tipoIndice = TipoIndices::GRIEGO;
+	estructTipoIndice.tipoEstructura = TipoIndices::ARBOL_BS;
+	estructTipoIndice.tamanioBloque = 2048;
+	
+	listaNombresClaves = new ListaNombresClaves();
+	listaNombresClaves->push_back("TITULO");
+	listaNombresClaves->push_back("DIRECTOR");
+	
+	nodoListaIndices.estructTipoIndice = estructTipoIndice;
+	nodoListaIndices.listaNombresClaves = listaNombresClaves;
+	
+	listaTiposIndices = new ListaTiposIndices();
+	listaTiposIndices->push_back(nodoListaIndices);
+	
+	
+	//INDICE ROMANO (ARBOLB+)
+	estructTipoIndice.tipoIndice = TipoIndices::ROMANO;
+	estructTipoIndice.tipoEstructura = TipoIndices::ARBOL_BP;
+	estructTipoIndice.tamanioBloque = 1024;
+	
+	listaNombresClaves = new ListaNombresClaves();
+	listaNombresClaves->push_back("GENERO");
+	
+	nodoListaIndices.estructTipoIndice = estructTipoIndice;
+	nodoListaIndices.listaNombresClaves = listaNombresClaves;
+	
+	listaTiposIndices->push_back(nodoListaIndices);
+	
+	//Se inserta la lista de tipos de indices en el mapa de tipos de indices
+	this->mapaTiposIndices["PELICULA"] = listaTiposIndices;
+
+}
+
+
+DefinitionsManager::ListaTiposAtributos* DefinitionsManager::getListaTiposAtributos(const string &nombreTipo) {
+	
+	return this->mapaTiposAtributos[nombreTipo];
+	
+}
+
+
+DefinitionsManager::ListaValoresAtributos* DefinitionsManager::getListaValoresAtributos(const string &nombreTipo,
+																	const MapaValoresAtributos &mapaValoresAtributos) {
+	
+	//Lista a retornar
+	ListaValoresAtributos* listaValoresAtributos = new ListaValoresAtributos();
+	
+	ListaNombresAtributos* listaNombresAtributos = this->mapaNombresAtributos[nombreTipo];
+	
+	MapaValoresAtributos::const_iterator iterMap;
+	
+	string valorAtributo("");
+	
+	for (ListaNombresAtributos::iterator iter = listaNombresAtributos->begin();
+		iter != listaNombresAtributos->end(); ++iter) {
+		
+		if ( (iterMap = mapaValoresAtributos.find(*iter)) != mapaValoresAtributos.end() )
+			valorAtributo = iterMap->second;
+		else valorAtributo = "";
+		
+		listaValoresAtributos->push_back(valorAtributo);
+		
+	}
+	
+	return listaValoresAtributos;
+	
+}
+
+
+DefinitionsManager::ListaTiposIndices* DefinitionsManager::getListaTiposIndices(const string &nombreTipo) {
+	
+	return this->mapaTiposIndices[nombreTipo];
+	
 }
 
--- trunk/CapaIndices/Common/DefinitionsManager.h
+++ trunk/CapaIndices/Common/DefinitionsManager.h
@@ -2,6 +2,7 @@
 #define DEFINITIONSMANAGER_H_
 
 #include "../RegisterInfo/RegisterInfo.h"
+#include "TipoIndices.h"
 #include <list>
 #include <map>
 
@@ -11,16 +12,18 @@ class DefinitionsManager {
 		//Definiciones
 		typedef ListaNodos ListaTiposAtributos;
 		
+		typedef std::list<std::string> ListaNombresAtributos;
+		
 		typedef std::list<std::string> ListaValoresAtributos;
 		
 		typedef struct _nodoListaClaves {
-			string nombreClave;
-			string valorClave;
+			std::string nombreClave;
+			std::string valorClave;
 		} NodoListaClaves;
 		
 		typedef std::list<NodoListaClaves> ListaClaves;
 		
-		typedef std::list<string> ListaNombresClaves;
+		typedef std::list<std::string> ListaNombresClaves;
 		
 		typedef struct _estructTipoIndice {
 			unsigned char tipoIndice;
@@ -39,6 +42,8 @@ class DefinitionsManager {
 		
 		typedef std::map<std::string, std::string> MapaValoresAtributos;
 		
+		typedef std::map<std::string, ListaNombresAtributos*> MapaNombresAtributos;
+		
 		typedef std::map<std::string, ListaTiposIndices*> MapaTiposIndices;
 		
 		//Metodos
@@ -46,14 +51,35 @@ class DefinitionsManager {
 		
 		virtual ~DefinitionsManager();
 		
+		ListaTiposAtributos* getListaTiposAtributos(const string &nombreTipo);
+		
+		//Devuelve una lista con los valores de los atributos ordenados segun el archivo de definiciones.
+		//Si 'insercion == true' significa que la lista de valores de atributos se utilizara en una alta,
+		//por lo cual el campo 'debeModificar' de cada nodo de la lista tendra el valor false.
+		//Si 'insercion == false' significa que la lista de valores de atributos se utilizara en una modificacion,
+		//por lo cual el campo 'debeModificar' de cada nodo de la lista que se haya encontrado en el mapa de valores
+		//tendra el valor true, y los nodos que no se encontraron en el mapa tendran el valor false.
+		ListaValoresAtributos* getListaValoresAtributos(const string &nombreTipo,
+														const MapaValoresAtributos &mapaValoresAtributos);
+		
+		ListaTiposIndices* getListaTiposIndices(const string &nombreTipo);
+		
 	private:
 		//Atributos
 		MapaTiposAtributos mapaTiposAtributos;
 		
 		MapaTiposIndices mapaTiposIndices;
+		
+		MapaNombresAtributos mapaNombresAtributos;
 
 		//Metodos
 		void cargarDefiniciones();
+		
+		void cargarNombresAtributos();
+		
+		void cargarTiposAtributos();
+		
+		void cargarTiposIndices();
 		
 };
 
