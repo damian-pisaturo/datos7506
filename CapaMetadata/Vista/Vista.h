b0ce4610c8ee5bd641cff999f624e60350a795a6
manugarciacab <manugarciacab@4ef8b2a0-b237-0410-b753-5dc98fa25004>
2007-11-14 05:59:54 +0000

    Clase utilizada para la vista.

---
+++ CapaMetadata/Vista/Vista.h
@@ -0,0 +1,35 @@
+#ifndef VISTA_H_
+#define VISTA_H_
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <list>
+
+#include "../../Common/DefinitionsManager.h"
+#include "../DataManager/DataManager.h"
+#include "../../CapaIndices/Bloque/Bloque.h"
+#include "../../Common/Tamanios.h"
+
+
+class Vista
+{
+public:
+	Vista();
+	virtual ~Vista();
+	/*
+	 * Muestra por consola el contenido de un registro
+	 * */
+	void showRegister(char *registro, ListaNodos *listaTiposAtributos);
+	
+	
+	/*
+	 * Muestra por consola los campos de un tipo correspondiente
+	 * */
+	void showTipoContent(list<string>& listaNombresCampos);
+	
+
+
+};
+
+#endif /*VISTA_H_*/
