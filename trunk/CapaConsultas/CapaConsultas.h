6ce18666857f2b76dc4d709094eaec6af0a382b3
manugarciacab <manugarciacab@4ef8b2a0-b237-0410-b753-5dc98fa25004>
2007-12-03 03:01:52 +0000

    Se comenzo a implementar el main de la capa de consultas.

--- trunk/CapaConsultas/CapaConsultas.h
+++ trunk/CapaConsultas/CapaConsultas.h
@@ -2,6 +2,16 @@
 #define CAPACONSULTAS_H_
 
 #include <iostream>
+#include "../Common/ResultadosConsultas.h"
+#include "../Common/ResultadosMetadata.h"
+#include "../CapaMetadata/Parser/ParserOperaciones.h"
+#include "../CapaMetadata/Vista/Vista.h"
+#include "../ComuDatos/ComuDatos.h"
+#include "../Common/Resultado.h"
+#include "../Common/ImpresorResultado.h"
+#include "../Common/NombresCapas.h"
+
+using namespace std;
 
 int main(int argc, char* argv[]);
 
---
+++ trunk/Common/ResultadosConsultas.h
@@ -0,0 +1,14 @@
+#ifndef RESULTADOSCONSULTAS_H_
+#define RESULTADOSCONSULTAS_H_
+
+class ResultadosConsultas
+{
+public:
+	static const char OK                 	= -75;
+	static const char OPERACION_INVALIDA 	= -76;
+	static const char NO_HAY_DEFINICIONES	= -77;
+	static const char FINALIZAR_PROGRAMA	= -78;
+	
+};
+
+#endif /*RESULTADOSCONSULTAS_H_*/
